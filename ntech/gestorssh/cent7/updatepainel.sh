de28bfc05c1c26c2f4c4ca47937034d49b493246
sudosalve <96689877+sudosalve@users.noreply.github.com>
2022-05-15 09:18:20 -0400

    Update updatepainel.sh

--- ntech/gestorssh/cent7/updatepainel.sh
+++ ntech/gestorssh/cent7/updatepainel.sh
@@ -43,7 +43,7 @@ dpkg-reconfigure --frontend noninteractive tzdata
 clear
 echo -e "\E[44;1;37m    ATUALIZANDO O PAINELWEB GESTOR-SSH     \E[0m"
 echo ""
-echo -e "                              \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                              \033[1;31mBy @GOMESNET\033[1;36m"
 echo -e "   GESTOR-SSH" | figlet
 echo ""
 clear
@@ -54,7 +54,7 @@ sleep 2
 echo -e "           \033[1;33m● \033[1;32mFINALIZANDO A ATUALIZAÇÃO, PODE DEMORAR \033[1;33m● \033[1;33mAGUARDE...\033[0m"
 cd /var/www/html || exit
 rm -rf *
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/gestorssh.zip > /dev/null 2>&1
+wget https://github.com/sudosalve/plusnssh/raw/ntech/gestorssh/gestorssh.zip > /dev/null 2>&1
 sleep 1
 unzip -o gestorssh.zip > /dev/null 2>&1
 rm -rf gestorssh.zip index.html > /dev/null 2>&1
@@ -70,7 +70,7 @@ clear
 IP=$(wget -qO- ipv4.icanhazip.com)
 clear
 echo ""
-echo -e "                              \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                              \033[1;31mBy @GOMESNET\033[1;36m"
 echo -e "   GESTOR-SSH" | figlet
 echo ""
 echo -e "\033[1;36m PAINELWEB DIGITE ESSE IP NO NAVEGADOR:\033[1;37m http://$IP/admin\033[0m"
